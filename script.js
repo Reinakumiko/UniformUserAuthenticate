2da3f5d9b0e2bbb6ca5d6a45243c7fea304ae619
Reinakumiko <1164114736@qq.com>
2016-10-13 17:49:47 +0800

    Add Animations

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-$(".loginway").click(function (){
-	
+
+$(document).ready(function (){
+	$(".loginWay").click(function (){
+		var way = $(this).attr("data-loginway");
+
+		console.log(way);
+
+		LoginPanelSwitch(way);
+	});
 });
+
+
+function LoginPanelSwitch(activeWay) {
+	var index = $("#"+activeWay).parent().index();
+	var offset = index * 500;
+	
+	$(".panelSlider").css({"transform": "translate3d(-" + offset + "px, 0, 0)"})
+}
